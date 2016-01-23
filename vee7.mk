5c914bb95b86e5c2c35110b150edf34fb597d2b3
neutrondev <neutrondev03@gmail.com>
2016-01-23 23:33:28 +0200

    Misstype

--- vee7.mk
+++ vee7.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product, vendor/lge/lge/vee7.mk)
+$(call inherit-product, vendor/lge/vee7.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/vee-common/overlay
 
