b60326b6954700f6d9adb2de729a5990465580fc
neutrondev <neutrondev03@gmail.com>
2016-02-02 14:42:12 +0200

    Changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,3 @@
-USE_CAMERA_STUB := true
-
 TARGET_BOOTLOADER_BOARD_NAME := vee7
 
 # inherit from the proprietary version
@@ -14,7 +12,6 @@ TARGET_GLOBAL_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_UIDS
 COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-COMMON_GLOBAL_CFLAGS += -DUSE_PRE_KITKAT_BLOBS
 
 # Targets
 TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
@@ -41,7 +38,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01300000
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_CONFIG := cyanogenmod_vee7_defconfig
-BOARD_KERNEL_CMDLINE := androidboot.hardware=vee7 msm_cpr.enable=0 androidboot.selinux=permissive no_console_suspend=1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=vee7 msm_cpr.enable=0 selinux=0 no_console_suspend=1
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
@@ -55,6 +52,8 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Qualcomm hardware
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+TARGET_USES_QCOM_BSP := true
+TARGET_USES_ION := true
 
 # GPS
 BOARD_USES_QCOM_LIBRPC := true
@@ -71,7 +70,6 @@ BOARD_USES_LEGACY_MMAP := true
 # Display
 BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 TARGET_NO_INITLOGO := true
 
@@ -84,6 +82,9 @@ BOARD_HARDWARE_CLASS := device/lge/vee7/cmhw
 # BT
 BOARD_HAVE_BLUETOOTH := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/vee7/bluetooth
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/lge/vee7/ril/
 
 # Light
 TARGET_PROVIDES_LIBLIGHT := true
@@ -110,9 +111,6 @@ EXTENDED_FONT_FOOTPRINT := true
 # Block based Over-The-Air
 BLOCK_BASED_OTA := false
 
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-
 # Wlan
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
@@ -122,6 +120,8 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_CFG80211_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_CFG80211_MODULE_NAME := "cfg80211"
 WIFI_EXT_MODULE_PATH := "/system/lib/modules/librasdioif.ko"
 WIFI_EXT_MODULE_NAME := "librasdioif"
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
@@ -131,33 +131,15 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WIFI_DRIVER_FW_PATH_PARAM := "/data/misc/wifi/fwpath"
 
-# Sepolicy
-#
-#BOARD_SEPOLICY_DIRS += device/lge/vee7/sepolicy
-#
-#BOARD_SEPOLICY_UNION += \
-#       device.te \
-#       file.te \
-#       file_contexts \
-#       healthd.te \
-#       init_shell.te \
-#       kernel.te \
-#       led_deamon.te \
-#       location.te \
-#       mediaserver.te \
-#       mm-qcamerad.te \
-#       property.te \
-#       property_contexts \
-#       sdcardd.te \
-#       sensors.te \
-#       system_app.te \
-#       system_server.te \
-#       vold.te
-
 # Bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # FSTAB
 TARGET_RECOVERY_FSTAB := device/lge/vee7/rootdir/fstab.vee7
+
+# Charger
+COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' -DBOARD_CHARGING_CMDLINE_VALUE='"charger"'
+
+
 	
