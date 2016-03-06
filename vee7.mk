de5e5ab15465d10d6e6ccc345973807e6fb3041a
neutrondev <neutrondev03@gmail.com>
2016-03-06 17:34:02 +0200

    Updated

--- vee7.mk
+++ vee7.mk
@@ -8,10 +8,10 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/vee7/overlay
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
-PRODUCT_LOCALES := en_US
-PRODUCT_LOCALES += hdpi
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Rootdir files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/lge/vee7/rootdir,root)
@@ -21,60 +21,35 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/lge/vee7/system,sys
 
 # Permission files
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+    
 # display HALS
 PRODUCT_PACKAGES += \
-    gralloc.msm7x27a \
     copybit.msm7x27a \
-    memtrack.msm7x27a \
-    hwcomposer.msm7x27a \
-    libgenlock \
+    gralloc.msm7x27a \
+	libgenlock \
 	liboverlay \
-    
+
+# OMX    
 PRODUCT_PACKAGES += \
     libmm-omxcore \
     libOmxCore \
-    libstagefrighthw \
-    libdashplayer
+    libstagefrighthw 
 
-# Hals
-PRODUCT_PACKAGES +=  \
-	gps.msm7x27a \
-	power.msm7x27a \
-	lights.msm7x27a \
-	camera.msm7x27a \
-	libhealthd.msm7x27a
-
-# BT
-PRODUCT_PACKAGES += \
-	libbt-vendor
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    make_ext4fs \
-    e2fsck \
-    resize2fs \
-    setup_fs
-
-PRODUCT_PACKAGES += \
-	com.android.future.usb.accessory \
-	Torch \
-	libinit_vee7
-
-# audio 
+# Audio 
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
 	audio.usb.default \
@@ -83,8 +58,36 @@ PRODUCT_PACKAGES += \
 	audio_policy.msm7x27a \
 	libaudio-resampler \
 	libaudioparameter \
-	libaudioutils
-	
+	libaudioutils 
+    
+# HAL
+PRODUCT_PACKAGES +=  \
+	gps.msm7x27a \
+	power.msm7x27a \
+	lights.msm7x27a \
+	camera.msm7x27a \
+	libhealthd.msm7x27a
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    e2fsck \
+    resize2fs \
+    setup_fs
+    
+# Live wallpaper
+PRODUCT_PACKAGES += \
+    librs_jni \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers
+    
+# Additional
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory \
+	Torch \
+	libinit_vee7
+
 # Qcom SoftAP
 PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg
